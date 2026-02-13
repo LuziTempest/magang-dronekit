9e4e4bd36437e22c3445855f50845cb3bd168c1f
Zulkarnaen <luzinaen@gmail.com>
2026-02-13 23:15:12 +0700

    UP

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-# magang-dronekit
+# General Flow:
+
+- cara menjalankan program saat sudah jadi secara general adalah
+  - setup mission planner dan run mission
+  - set mission planner menjadi tcp host agar dapat terkoneksi dengan backend
+  - jalankan backend
+  - jalankan frontend
+
+# Setup Backend
+
+1. Clone repo ini
+2. Jalankan `python -m venv venv` lalu install libraries dengan `pip install -r requirements.txt`
+3. Ubah hal berikut di package dronekit `venv/Lib/site-packages/dronekit/__init__.py`,
+
+- tambahkan kode berikut di line 38
+
+```py
+import sys
+if sys.version_info.major == 3 and sys.version_info.minor >=  10:
+    from collections.abc import MutableMapping
+else:
+    from collections import MutableMapping
+```
+
+- lalu ubah di file yang sama sekitar line 2694, dari
+
+```py
+class Parameters(collections.MutableMapping, HasObservers)
+```
+
+menjadi
+
+```py
+class Parameters(MutableMapping, HasObservers)
+```
+
+selesai, selamat ngoding backend!
+
+# Setup Mission Planner
+
+Buka mission planner dan connect ke plane seperti biasanya
+
+Lalu ctrl+f dan pilih mavlink, terdapat di kolom paling kiri urutan ke 6 dari atas
+
+![conf](./img/conf.png)
+
+Setelah itu set sebagai berikut
+
+![host](./img/image.png)
+Setelah semuanya telah disetup, bisa langsung jalankan backend dan setelah itu buka frontend
+
+# Extension di VSCode untuk mempermudah pengerjaan
+
+### Thunder Client
+
+![logo](img/logo.png)
+
+untuk memudahkan saat mendevelop api / server. Kita bisa testing endpoint yang sudah kita buat langsung di vscode dengan menggunakan thunder client
+
+# Referensi
+- https://ardupilot.org/dev/docs/mavlink-commands.html
+- https://mavlink.io/en/messages/common.html
+- https://dronekit-python.readthedocs.io/en/latest/
+- https://ardupilot.org/plane/docs/parameters.html
+- https://ardupilot.org/plane/docs/flight-modes.html
+- https://dronekit-python.readthedocs.io/en/latest/guide/auto_mode.html#mission-command-overview
---
+++ app.py
@@ -0,0 +1,135 @@
+from flask import Flask, jsonify, render_template, request, redirect 
+from pymavlink import  mavutil
+from dronekit import connect, Command, LocationGlobalRelative, VehicleMode
+import math
+
+app = Flask(__name__)
+try:
+    vehicle = connect("tcp:127.0.0.1:14550", wait_ready=True)
+    vehicle.wait_ready("autopilot_version")
+except Exception as e:
+    print(f"Gagal Connect, {e}")
+    
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    if request.method == "POST":
+        param1 = request.form.get('min_acc')
+        try:
+            if param1:
+                vehicle.parameters['TKOFF_THR_MINACC'] = float(param1)
+            print("Parameter berhasil diupdate ke Drone via MAVLink!")
+        except Exception as e:
+            print(f"Gagal update parameter: {e}")
+        return render_template('app.html', param1=param1, mode=vehicle.mode.name, arm=vehicle.armed)
+    else:
+        p1 = vehicle.parameters.get('TKOFF_THR_MINACC', 0)
+        return render_template("app.html", param1=p1, mode=vehicle.mode.name, arm=vehicle.armed)
+    
+
+@app.route("/get_all_params", methods=['GET'])
+def get_all_params():
+    try:
+        params = {param: value for param, value in vehicle.parameters.items()}        
+        return jsonify(params), 402
+    except Exception as e:
+        return jsonify(ok=False)
+
+@app.route("/plane_status", methods=['GET'])
+def cek_params():
+    print(f"Autopilot Firmware version: {vehicle.version}")
+    print(f"Autopilot capabilities (supports ftp): {vehicle.capabilities.ftp}")
+    print(f"Global Location: {vehicle.location.global_frame}")
+    print(f"Global Location (relative altitude): {vehicle.location.global_relative_frame}")
+    print(f"Local Location: {vehicle.location.local_frame}") # NED
+    print(f"Attitude: {vehicle.attitude}")
+    print(f"Velocity: {vehicle.velocity}")
+    print(f"GPS: {vehicle.gps_0}")
+    print(f"Groundspeed: {vehicle.groundspeed}")
+    print(f"Airspeed: {vehicle.airspeed}")
+    print(f"Gimbal status: {vehicle.gimbal}")
+    print(f"Battery: {vehicle.battery}")
+    print(f"EKF OK?: {vehicle.ekf_ok}")
+    print(f"Last Heartbeat: {vehicle.last_heartbeat}")
+    print(f"Heading: {vehicle.heading}")
+    print(f"Is Armable?: {vehicle.is_armable}")
+    print(f"System status: {vehicle.system_status.state}")
+    print(f"Current WP: {vehicle._current_waypoint}")
+    print(f"Type: {vehicle.version.vehicle_type}")
+    print(f"Mode: {vehicle.mode.name}")
+    print(f"Armed: {vehicle.armed}")
+    
+    return redirect("/")
+
+# Mengubah meter -> koordinat
+def get_location_metres(original_location, dNorth, dEast):
+    """
+    Menghitung lokasi baru berdasarkan jarak (meter) dari lokasi awal.
+    """
+    earth_radius = 6378137.0 # Radius bumi dalam meter
+    
+    # Pergeseran koordinat dalam radian
+    dLat = dNorth / earth_radius
+    dLon = dEast / (earth_radius * math.cos(math.pi * original_location.lat / 180))
+
+    # Lokasi baru dalam derajat
+    newlat = original_location.lat + (dLat * 180 / math.pi)
+    newlon = original_location.lon + (dLon * 180 / math.pi)
+    
+    return LocationGlobalRelative(newlat, newlon, original_location.alt)
+
+@app.route("/wp", methods=['GET'])
+def wp():
+    cmds = vehicle.commands
+    cmds.download()
+    cmds.wait_ready()
+    cmds.clear()
+
+    # Parameter: (target_sys, target_comp, seq, frame, command, current, autocontinue, p1, p2, p3, p4, lat, lon, alt)
+    home_point = vehicle.home_location
+    cmd1 = Command(0, 0, 0, 
+                mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, 
+                mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 
+                0, 0, 0, 0, 0, 0, home_point.lat, home_point.lon, 10)
+
+    cmd2 = Command(0, 0, 0, 
+                mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, 
+                mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 
+                0, 0, 0, 0, 0, 0, -7.6174426, 112.8457743, 20)
+    
+    target_wp = get_location_metres(home_point, 100, 100)
+    
+    cmd3 = Command(0, 0, 0, 
+                    mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, 
+                    mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 
+                    0, 0, 0, 0, 0, 0, 
+                    target_wp.lat, target_wp.lon, 10)
+    
+    
+    cmds.add(cmd1)
+    cmds.add(cmd2)
+    cmds.add(cmd3)
+    
+    print("Uploading missions...")
+    cmds.upload() 
+
+    try:
+        return redirect('/')
+    except Exception as e:
+        return redirect('/')
+
+@app.route("/arm", methods=['GET', 'POST'])
+def arm():
+    if request.method == "POST":
+        if vehicle.armed:
+            vehicle.disarm()
+            print("Berhasil Disarming!")
+        else:
+            vehicle.arm()
+            print("Berhasil Arming!")
+        return redirect('/')
+    else:
+        return redirect('/')
+    
+    
+app.run(debug=True)
